73ad5c6a6d8d09760aeeb1bf266b658562b63fe2
Cameron <mooreheadc@hotmail.com>
2016-08-20 12:04:20 -0700

    sequenceInterface.js partially solved

--- ch6/sequenceInterface.js
+++ ch6/sequenceInterface.js
@@ -1,31 +1,55 @@
-var iterator = function(sequence, fn) {
-    if (typeof sequence === 'object' && sequence !== null)
-        var keys = Object.keys(sequence);
-    else if (sequence === null)
-        return console.log('cannot iterate over null value');
-    else
-        var keys = sequence;
+'use strict'
 
-    for (var i = 0; i < keys.length; i++)
-        fn(sequence, keys, i);
+function arraySeq(array) {
+    this.array = array
+    this.i = 0
+    this.counter = array.length
+    this.next = function() {
+        let current = array[this.i]
+        this.i++
+        this.counter--
+        return current
+    }
+    this.remainingElements = function() {
+        return this.counter
+    }
+}
+function rangeSeq(x, y) {
+    this.i = x
+    this.counter = y - x + 1
+    this.next = function() {
+        let current = x
+        this.i = x++
+        this.counter --
+        return current
+    }
+    this.remainingElements = function() {
+        return this.counter
+    }
 }
 
-logFive.prototype.test = "testing!";
-
-function logFive(sequence, elements, i) {
-    if (i < 5)
-        console.log(sequence[elements[i]]);
+function logFive(obj) {
+    obj.counter = 5
+    do {
+        console.log(obj.next())
+    } while
+        (obj.remainingElements())
 }
 
-var sequenceObject1 = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6
-}
+var testArray = new arraySeq([1,2,3])
+var testArray2 = new arraySeq([2,4,6,8,10,12,14,16,18,20])
+var testArray3 = new arraySeq([10,20,30,40,50])
 
-iterator(sequenceObject1, logFive);
-iterator(null, logFive);
+var testRange = new rangeSeq(1, 3)
+var testRange2 = new rangeSeq(10, 20)
+var testRange3 = new rangeSeq(1, 5)
 
+console.log("Array Sequencing:")
+console.log(logFive(testArray))
+console.log(logFive(testArray2))
+console.log(logFive(testArray3))
+
+console.log("Range Sequencing:")
+console.log(logFive(testRange))
+console.log(logFive(testRange2))
+console.log(logFive(testRange3))
