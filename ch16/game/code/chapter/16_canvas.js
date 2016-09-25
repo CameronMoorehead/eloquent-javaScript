1b74d8d575e1cf0ca30bf63e65de65791b5088fc
Cameron <mooreheadc@hotmail.com>
2016-09-25 14:46:38 -0700

    shapes.html partially solved

--- ch16/game/code/chapter/16_canvas.js
+++ ch16/game/code/chapter/16_canvas.js
@@ -1,10 +1,3 @@
-var results = [
-  {name: "Satisfied", count: 1043, color: "lightblue"},
-  {name: "Neutral", count: 563, color: "lightgreen"},
-  {name: "Unsatisfied", count: 510, color: "pink"},
-  {name: "No comment", count: 175, color: "silver"}
-];
-
 function flipHorizontally(context, around) {
   context.translate(around, 0);
   context.scale(-1, 1);
